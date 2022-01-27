01d8f82d0933f249a3216485905fe361986d2ef4
jbbranch9 <jacob.b.branch1@gmail.com>
2022-01-26 23:42:27 -0800

    added main.tscn

---
+++ main.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+"""
+main.tscn is the launch point for the entire program.
+	All player interfaces should enter the scene hierarchy here.
+	
+game.tscn is an instance of the primary gameplay loop.
+	It should be able to run as a standalone scene.
+	In the finished game, this is what will be called any time a "New Game" button is clicked.
+"""
